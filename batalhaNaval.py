49954e7b331e01f256b24aaac6ff64c36b026145
PedroStabach <pedroaugustostabach3@gmail.com>
2025-05-28 22:27:27 -0300

    posicoes funcionando

--- batalhaNaval.py
+++ batalhaNaval.py
@@ -11,24 +11,25 @@ def navios():
     count = 4
     for i in range(3):
         print(f"escolha o navio com {count} posicoes")
-        colocar_navios(matriz, count)
+        colocar_navios(matriz, 1, count)
         count -= 1
         mostrar_matriz(matriz)
 
-def posicoes(x, y):
-    posicoes = []
-    posicoes.append(x,y)
-
-def colocar_navios(matriz, quantidade):
+def colocar_navios(matriz, quantidade, count):
     for i in range(quantidade):
         while True:
             try:
-                entrada = input(f"Escolha a posição {i+1} do seu navio (x y): ")
+                entrada = input(f"Escolha a posição inicial do seu navio (x y): ")
+                saida = input(f"escolha a posicao final do seu navio (x,y): ")
                 x, y = map(int, entrada.split())
-                if 0 <= x < len(matriz) and 0 <= y < len(matriz):
+                xf, yf = map(int, saida.split())
+                if (0 <= x < len(matriz) and 0 <= y < len(matriz)) and (0 <= xf < len(matriz) and 0 <= yf < len(matriz)):
                     if matriz[x][y] == "-":
-                        matriz[x][y] = "N"  # Marca o navio
-                        posicoes(x,y,)
+                        if x == xf:
+                            for i in range(count):
+                                matriz[x][(y + i)] = "N"
+                                var = f"{x}, {y + i}"
+                                posicao.append(var)
                         break
                     else:
                         print("Já existe um navio nessa posição. Tente novamente.")
@@ -38,9 +39,10 @@ def colocar_navios(matriz, quantidade):
                 print("Entrada inválida. Digite dois números separados por espaço.")
 
 # Programa principal
-
 tamanho = 10
+posicao = []
 matriz = criar_matriz(tamanho)
 mostrar_matriz(matriz)
 navios()
-## Agora preciso que
+print(posicao)
+#atualmente eu faco a lista e armazeno as posicoes, agora tenho que fazer o seguinte, validacao das posicoes E comecar o jogo contra o bot
